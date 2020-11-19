8244771232fe66c71b837c0a16cbde620f8eae5b
Lee-YuGyeong <60072190+Lee-YuGyeong@users.noreply.github.com>
2020-11-19 18:56:36 +0900

    home

--- app/src/main/java/com/example/mindfood/MainActivity.java
+++ app/src/main/java/com/example/mindfood/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -31,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+
     }
 
 }
--- app/src/main/java/com/example/mindfood/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/mindfood/ui/home/HomeFragment.java
@@ -9,7 +9,9 @@ import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.example.mindfood.MainActivity;
 import com.example.mindfood.R;
+import com.example.mindfood.ui.myDrawer.MyDrawerFragment;
 
 public class HomeFragment extends Fragment {
 
@@ -18,6 +20,30 @@ public class HomeFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        Button button = (Button) root.findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, new MyDrawerFragment()).addToBackStack(null).commit();
+            }
+        });
+
+        Button button2 = (Button) root.findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, new MyDrawerFragment()).addToBackStack(null).commit();
+            }
+        });
+
+        Button button3 = (Button) root.findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, new MyDrawerFragment()).addToBackStack(null).commit();
+            }
+        });
 
         return root;
     }
